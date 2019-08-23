97ec2e4f462c22bf8dadd97859d4cb31a1066d14
Rob J Hyndman <robjhyndman@gmail.com>
2019-08-23 15:07:06 +1000

    Simplified life_table function

--- R/life_table.R
+++ R/life_table.R
@@ -47,125 +47,66 @@ life_table <- function(.data, age, mortality, group) {
     }
   }
   # Create life table for each sub-tibble and row-bind them.
-  purrr::map_dfr(lst_data, function(dt) {
-    lt(dt$mx, startage=min(dt$x), agegroup = dt$x[2]-dt$x[1],
-       sex=dt$group[1]) %>%
-    tibble::rownames_to_column("Age") %>%
-    mutate(Year = dt$year[1], Group=dt$group[1])
-  }) %>%
+  purrr::map_dfr(lst_data, function(dt) { lt(dt) }) %>%
     as_tsibble(index=Year, key=c(Age, Group)) %>%
     dplyr::select(Year, Age, Group, dplyr::everything()) %>%
-    mutate(Age = as.numeric(Age)) %>%
     arrange(Group, Year, Age)
 }
 
-lt <- function(mx, startage = 0, agegroup = 5, sex) {
-  # Omit missing ages
-  if (is.na(mx[1])) {
-    mx[1] <- 0
-  }
-  firstmiss <- (1:length(mx))[is.na(mx)][1]
-  if (!is.na(firstmiss)) {
-    mx <- mx[1:(firstmiss - 1)]
-  }
-  nn <- length(mx)
-  if (nn < 1) {
-    stop("Not enough data to proceed")
+lt <- function(dt) {
+  # Grab information from tibble
+  mx <- dt$mx
+  startage <- round(min(dt$x))
+  agegroup <- round(dt$x[2]-dt$x[1])
+  sex <- tolower(dt$group[1])
+
+  if (agegroup == 5L & startage > 0L & startage < 5L)
+    stop("0 < startage < 5 not supported for 5-year age groups")
+  else if(startage < 0L)
+    stop("startage must be non-negative")
+  else if(agegroup != 1L & agegroup != 5L)
+    stop("Only 1-year and 5-year agegroups handled")
+
+  # Set a0
+  if (startage == 0L) {
+    a0 <- case_when(
+        sex == "female" ~ 0.35 + (mx[1]<0.107)*(-0.297 + 2.8 * mx[1]),
+        sex == "male"   ~ 0.33 + (mx[1]<0.107)*(-0.285 + 2.684 * mx[1]),
+        TRUE            ~ 0.34 + (mx[1]<0.107)*(-0.291 + 2.742 * mx[1])
+      )
+  } else {
+    a0 <- 0.5
   }
 
   # Compute width of each age group
-  if (agegroup == 1) {
-    nx <- c(rep(1, nn - 1), Inf)
-  } else if (agegroup == 5) { # First age group 0, then 1-4, then 5-year groups.
-    nx <- c(1, 4, rep(5, nn - 3), Inf)
-  } else {
-    stop("agegroup must be either 1 or 5")
-  }
+  nn <- NROW(dt)
+  nx <- c(1L, rep(agegroup, nn-2), Inf)
+  if(agegroup == 5L)
+    nx[2] <- 4L
 
-  if (agegroup == 5 & startage > 0 & startage < 5) {
-    stop("0 < startage < 5 not supported for 5-year age groups")
-  }
-
-  if (startage == 0) # for single year data and the first age (0) in 5-year data
-  {
-    if (sex == "female") {
-      if (mx[1] < 0.107) {
-        a0 <- 0.053 + 2.8 * mx[1]
-      } else {
-        a0 <- 0.35
-      }
-    }
-    else if (sex == "male") {
-      if (mx[1] < 0.107) {
-        a0 <- 0.045 + 2.684 * mx[1]
-      } else {
-        a0 <- 0.33
-      }
-    }
-    else # if(sex == "total")
-    {
-      if (mx[1] < 0.107) {
-        a0 <- 0.049 + 2.742 * mx[1]
-      } else {
-        a0 <- 0.34
-      }
-    }
-  }
-  else if (startage > 0) {
-    a0 <- 0.5
-  } else {
-    stop("startage must be non-negative")
-  }
-  if (agegroup == 1) {
+  # Set remaining ax values
+  if (agegroup == 1L) {
     if (nn > 1) {
-      ax <- c(a0, rep(0.5, nn - 2), Inf)
+      ax <- c(a0, rep(0.5, nn - 2L), Inf)
     } else {
       ax <- Inf
     }
   }
-  else if (agegroup == 5 & startage == 0) {
-    if (sex == "female") {
-      if (mx[1] < 0.107) {
-        a1 <- 1.522 - 1.518 * mx[1]
-      } else {
-        a1 <- 1.361
-      }
-    }
-    else if (sex == "male") {
-      if (mx[1] < 0.107) {
-        a1 <- 1.651 - 2.816 * mx[1]
-      } else {
-        a1 <- 1.352
-      }
-    }
-    else # sex == "total"
-    {
-      if (mx[1] < 0.107) {
-        a1 <- 1.5865 - 2.167 * mx[1]
-      } else {
-        a1 <- 1.3565
-      }
-    }
-    ax <- c(a0, a1, rep(2.6, nn - 3), Inf)
-    ### ax=2.5 known to be too low esp at low levels of mortality
-  }
-  else # agegroup==5 and startage > 0
-  {
+  else if (agegroup == 5L & startage == 0) {
+    a1 <- case_when(
+      sex == "female" ~ 1.361 + (mx[1]<0.107)*(0.161 - 1.518 * mx[1]),
+      sex == "male"   ~ 1.352 + (mx[1]<0.107)*(0.299 - 2.816 * mx[1]),
+      TRUE            ~ 1.3565+ (mx[1]<0.107)*(0.230 - 2.167 * mx[1])
+    )
+    ax <- c(a0, a1, rep(2.6, nn - 3L), Inf)
+  } else { # agegroup==5 and startage > 0
     ax <- c(rep(2.6, nn - 1), Inf)
-    nx <- c(rep(5, nn))
+    nx[1L] <- agegroup
   }
+  # Find qx
   qx <- nx * mx / (1 + (nx - ax) * mx)
-  # age <- startage + cumsum(nx) - 1
-  # if (max(age) >= 75) {
-  #    idx <- (age >= 75)
-  #   ax[idx] <- (1/mx + nx - nx/(1 - exp(-nx * mx)))[idx]
-  #  qx[idx] <- 1 - exp(-nx * mx)[idx]
-  #    }
-  # qx[qx > 1] <- 1  ################  NOT NEEDED IN THEORY
-
-  # plot(qx)  #### TO CHECK RESULT RE QX>1
-
   qx[nn] <- 1
+  # Find lx and dx
   if (nn > 1) {
     lx <- c(1, cumprod(1 - qx[1:(nn - 1)]))
     dx <- -diff(c(lx, 0))
@@ -173,10 +114,12 @@ lt <- function(mx, startage = 0, agegroup = 5, sex) {
   else {
     lx <- dx <- 1
   }
+  # Now Lx, Tx and ex
   Lx <- nx * lx - dx * (nx - ax)
   Lx[nn] <- lx[nn] / mx[nn]
   Tx <- rev(cumsum(rev(Lx)))
   ex <- Tx / lx
+  # Finally compute rx
   if (nn > 2) {
     rx <- c(Lx[1] / lx[1], Lx[2:(nn - 1)] / Lx[1:(nn - 2)], Tx[nn] / Tx[nn - 1])
   } else if (nn == 2) {
@@ -184,15 +127,18 @@ lt <- function(mx, startage = 0, agegroup = 5, sex) {
   } else {
     rx <- c(Lx[1] / lx[1])
   }
-  if (agegroup == 5) {
+  if (agegroup == 5L) {
     rx <- c(
       0, (Lx[1] + Lx[2]) / 5 * lx[1], Lx[3] / (Lx[1] + Lx[2]),
       Lx[4:(nn - 1)] / Lx[3:(nn - 2)], Tx[nn] / Tx[nn - 1]
     )
   }
-  result <- data.frame(
-    ax = ax, mx = mx, qx = qx, lx = lx,
-    dx = dx, Lx = Lx, Tx = Tx, ex = ex, rx = rx, nx = nx
-  )
+  # Return the results in a tibble
+  result <- tibble::tibble(
+      ax = ax, mx = mx, qx = qx, lx = lx,
+      dx = dx, Lx = Lx, Tx = Tx, ex = ex, rx = rx, nx = nx
+    ) %>%
+    mutate(Age = dt$x, Year = dt$year[1], Group=dt$group[1])
+
   return(result)
 }
