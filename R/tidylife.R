97f7ced8053420311da2084694a7bff806262112
Rob J Hyndman <robjhyndman@gmail.com>
2019-08-25 21:41:45 +1000

    Added more imports

--- R/tidylife.R
+++ R/tidylife.R
@@ -1,8 +1,12 @@
-#' @importFrom tsibble as_tsibble
-#' @importFrom tibble as_tibble
-#' @importFrom tidyr gather spread
 #' @importFrom dplyr mutate rename select arrange
 #' @importFrom magrittr %>%
+#' @importFrom purrr map2
+#' @importFrom rlang syms
+#' @importFrom stats na.omit
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr gather spread
+#' @importFrom tidyselect everything
+#' @importFrom tsibble as_tsibble
 #'
 NULL
 
