d5d291dc435d66767de925ae647f58383a086da6
Rob J Hyndman <robjhyndman@gmail.com>
2023-09-02 15:06:21 +0200

    Updated aus_mortality data

--- data-raw/australia.R
+++ data-raw/australia.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(vital)
+library(tsibble)
 
 aus_fertility <- as_vital(addb::aus.fertility) |>
   as_tibble() |>
@@ -21,8 +22,71 @@ aus_mortality <- bind_rows(
   as_vital(addb::wa) |> mutate(State = "Western Australia", Code = "WA") |> as_tibble()
 ) |>
   select(-AgeGroup) |>
-  as_tsibble(index = Year, key = all_of(c(Age, Sex, State, Code))) |>
+  as_tsibble(index = Year, key = c("Age", "Sex", "State", "Code")) |>
   select(Year, Age, Sex, State, Code, everything()) |>
   arrange(State, Sex, Year, Age) |>
   as_vital(.age = "Age", .sex = "Sex", .deaths = "Deaths", .population = "Exposure")
+
+# Updated data
+
+get_aus_mortality <- function(code) {
+  deaths <- read.table(
+    paste0("https://aushd.org/assets/txtFiles/humanMortality/",code,"/Deaths_1x1.txt"),
+    skip = 2, header = TRUE
+  ) |>
+    as_tibble() |>
+    tidyr::pivot_longer(Female:Total, names_to = "Sex", values_to = "Deaths") |>
+    mutate(Code = code)
+ read.table(
+    paste0("https://aushd.org/assets/txtFiles/humanMortality/",code,"/Exposures_1x1.txt"),
+    skip = 2, header = TRUE
+  ) |>
+    as_tibble() |>
+    tidyr::pivot_longer(Female:Total, names_to = "Sex", values_to = "Exposure") |>
+    left_join(deaths) |>
+    mutate(
+      Mortality = Deaths / Exposure,
+      Age = stringr::str_remove(Age, "\\+"),
+      Age = parse_integer(Age)
+    ) |>
+    select(Year, Age, Sex, Code, Mortality, Exposure, Deaths) |>
+    arrange(Sex, Year, Age) |>
+    as_vital(index = Year, key = c(Age, Sex, Code),
+             .age = "Age", .sex = "Sex", .deaths = "Deaths", .population = "Exposure") |>
+   collapse_ages()
+}
+
+state_names <- aus_mortality |>
+  as_tibble() |>
+  select(State, Code) |>
+  distinct() |>
+  bind_rows(
+    tribble(~State, ~Code,
+            "Australian Capital Territory", "ACT")
+  )
+
+aus_mort <- bind_rows(
+  get_aus_mortality("ACT"),
+  get_aus_mortality("NSW"),
+  get_aus_mortality("NT"),
+  get_aus_mortality("QLD"),
+  get_aus_mortality("SA"),
+  get_aus_mortality("TAS"),
+  get_aus_mortality("VIC"),
+  get_aus_mortality("WA"),
+) |>
+  arrange(Code, Sex, Year, Age) |>
+  mutate(Sex = tolower(Sex)) |>
+  left_join(state_names) |>
+  select(Year, Age, Sex, State, Code, everything())
+
+aus_mortality <- aus_mortality |>
+  filter(
+    Year < min(aus_mort$Year) | Code == "ACTOT",
+    Code != "AUS"
+  ) |>
+  bind_rows(aus_mort) |>
+  arrange(Sex, Year, Code, Age) |>
+  mutate(Mortality = if_else(Deaths == 0 & Exposure == 0, 0, Mortality))
+
 usethis::use_data(aus_mortality, overwrite = TRUE)
