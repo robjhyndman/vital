ee1d07f3e7e15b05e37b2231ce05c302193e1b60
Rob Hyndman <robjhyndman@gmail.com>
2024-04-13 17:06:39 +1000

    Make collapse_ages more robust

--- R/collapse_ages.R
+++ R/collapse_ages.R
@@ -35,10 +35,11 @@ collapse_ages <- function(.data, max_age = 100) {
   rates <- find_measures(.data, c("mx", "mortality", "fx", "fertility", "rate"))
 
   # Collapse data by summing above max_age
+  orig_data <- .data
   ages <- sort(unique(.data[[age]]))
   collapsed <- .data |>
     as_tibble() |>
-    dplyr::group_by_at(dplyr::vars(c(index, keys_noage))) |>
+    dplyr::group_by_at(c(index, keys_noage)) |>
     dplyr::reframe(dplyr::across(everything(),
       function(x) { collapse_age_vector(x, ages, max_age) } )) |>
     as_tsibble(index = index, key = all_of(c(keys_noage, age)))
@@ -47,12 +48,14 @@ collapse_ages <- function(.data, max_age = 100) {
   for(i in rates) {
     if(tolower(i) %in% c("mx", "mortality")) {
       if(length(deaths) == 0L | length(pop) == 0L) {
+        collapsed[[i]] <- orig_data[[i]][seq(nrow(collapsed))]
         warning(paste(i, "not recomputed"))
       } else {
         collapsed[[i]] <- collapsed[[deaths]] / collapsed[[pop]]
       }
     } else if(tolower(i) %in% c("fx", "fertility")) {
       if(length(births) == 0L | length(pop) == 0L) {
+        collapsed[[i]] <- orig_data[[i]][seq(nrow(collapsed))]
         warning(paste(i, "not recomputed"))
       } else {
         collapsed[[i]] <- collapsed[[births]] / collapsed[[pop]]
@@ -61,24 +64,33 @@ collapse_ages <- function(.data, max_age = 100) {
   }
 
   # Return result
+  colnames <- colnames[colnames %in% colnames(collapsed)]
   return(as_vital(collapsed, .age = age, .sex = sex,
     .deaths = deaths, .births = births, .population = pop)[,colnames]
   )
 }
 
 collapse_age_vector <- function(x, ages, max_age) {
-  # is it an age variable?
   if(is.numeric(x)) {
+    # is it an age variable? Keep it as is
     if(is.constant(diff(x))) {
-      x[ages <= max_age]
+      out <- x[ages <= max_age]
     } else {
-      c(x[ages < max_age], sum(x[ages >= max_age]))
+      # Sum upper group
+      out <- c(x[ages < max_age], sum(x[ages >= max_age]))
     }
   } else if(is.character(x)) {
+    # Probably AgeGroup. Add + to upper group
     out <- x[ages <= max_age]
     out[length(out)] <- paste0(out[length(out)],"+")
-    return(out)
+  } else if(is.logical(x)) {
+    # Perhaps OpenInterval variable
+    out <- c(x[ages < max_age], any(x[ages > max_age]))
+  } else {
+    # No idea what this is, but just truncate
+    out <- x[ages <= max_age]
   }
+  return(out)
 }
 
 is.constant <- function(x) {
--- tests/testthat/test-collapse_ages.R
+++ tests/testthat/test-collapse_ages.R
@@ -12,5 +12,11 @@ test_that("collapse_ages", {
       filter(Age == 100) |>
       pull(Mortality)
     expect_true(sum(abs(up1-up2)) == 0)
+    # Check that collapse ages works without population data
+    expect_warning(as_vital(fr.mort) |>
+      select(Year:Mortality) |>
+      collapse_ages())
+    # Check that collapse ages handles OpenInterval column from HMD
+    expect_warning(read_hmd_files("Mx_1x1.txt") |> collapse_ages())
   }
 })
