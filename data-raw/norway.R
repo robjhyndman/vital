0a826fb156f6b7c1549bacccb2047fef1ce1a1f2
Rob Hyndman <robjhyndman@gmail.com>
2024-05-13 17:06:24 +1000

    Remove NAs from norway_mortality

--- data-raw/norway.R
+++ data-raw/norway.R
@@ -1,5 +1,8 @@
 # HMD Norwegian data as downloaded from mortality.org on 21 April 2024
-norway_mortality <- read_hmd_files(here::here("data-raw",c("Population.txt", "Mx_1x1.txt")))
+norway_mortality <- read_hmd_files(here::here("data-raw",c("Population.txt", "Mx_1x1.txt"))) |>
+  mutate(
+    Mortality = if_else(is.na(Mortality) & Population == 0, 0, Mortality)
+  )
 norway_births <- read_hmd_files(here::here("data-raw","Births.txt"))
 norway_fertility <- read_hfd_files(here::here("data-raw","NORasfrRR.txt")) |>
   rename(Fertility = ASFR)
