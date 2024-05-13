5833e617d882354e036252a1205dfd8d38fd7223
Rob Hyndman <robjhyndman@gmail.com>
2024-05-13 17:36:35 +1000

    Working on a test for coherent forecasts

--- tests/testthat/test-coherent-forecast.R
+++ tests/testthat/test-coherent-forecast.R
@@ -4,9 +4,11 @@ library(ggplot2)
 
 # Regular forecasts
 nor <- norway_mortality |>
-  filter(Sex != "Total")
+  filter(Sex != "Total") |>
+  collapse_ages()
 fit1 <- nor |>
   model(fdm = FDM(log(Mortality)))
+autoplot(fit1)
 fc1 <- fit1 |>
   forecast(h=20)
 fc1 |>
@@ -15,11 +17,15 @@ fc1 |>
 # Product ratio forecasts
 pr <- nor |>
   make_pr(Mortality)
+nor |> autoplot() + scale_y_log10()
+pr |> autoplot(Mortality) + scale_y_log10()
+
 fit2 <- pr |>
   model(fdm = FDM(log(Mortality), coherent = TRUE))
+autoplot(fit2)
 fc_pr <- fit2 |>
   forecast(h=20)
-fc_pr |> autoplot()
+fc_pr |> autoplot() + scale_y_log10()
 fc2 <- fc_pr |> undo_pr(Mortality)
 fc2 |>
   autoplot() + scale_y_log10()
