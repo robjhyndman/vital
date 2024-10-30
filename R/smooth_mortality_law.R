e920046cb2a921f1b18ada0f7b78784cdfdba5eb
Rob Hyndman <robjhyndman@gmail.com>
2024-10-30 16:46:22 +1100

    Removed smooth_law_vital, simplified code and fixed documentation

--- R/smooth_mortality_law.R
+++ R/smooth_mortality_law.R
@@ -1,102 +1,52 @@
 #' Function to smooth mortality rates using MortalityLaw package
 #'
-#' This smoothing function allows smoothing of a variable in a vital object using MortalityLaw package.
+#' This smoothing function allows smoothing of a variable in a vital object using
+#' the MortalityLaw package.
 #' The vital object is returned along with some additional columns containing
-#' information about the smoothed variable: usually `.smooth` containing the
+#' information about the smoothed variable: `.smooth` containing the
 #' smoothed values, and `.smooth_se` containing the corresponding standard errors.
 #'
-#' `smooth_mortality_law()` smooths mortality rates using specified mortality law and optimisation method.
 #' @param .data A vital object
-#' @param .var name of variable to smooth
-#' @param law name of mortality law. For available mortality laws, users can check the \code{\link{MortalityLaws::availableLaws}}
+#' @param .var name of variable to smooth. This should contain mortality rates.
+#' @param law name of mortality law. For available mortality laws, users can check the \code{\link[MortalityLaws]{availableLaws}}. Argument ignored if a custom law supplied.
 #' function to learn about the available options.
-#' @param opt.method optimisation method. The models can be fitted under the maximum likelihood methodology
-#' or by selecting a loss function to be optimised. See the implemented loss function by running the \code{\link{MortalityLaws::xavailableLF}} function.
+#' @param ... Additional arguments are passed to \code{\link[MortalityLaws]{MortalityLaw}}.
 #' @return vital with added columns containing smoothed values and their standard errors
 #' @keywords smooth, mortality
-#' @rdname smooth_law_vital
 #' @author Sixian Tang and Rob J Hyndman
 #' @examples
 #' norway_mortality |> smooth_mortality_law(Mortality)
 #'
 #' @export
 smooth_mortality_law <- function(.data, .var, law = "gompertz", ...) {
-  smooth_law_vital(.data, {{ .var }}, smooth_mortality_law_x, law, ...)
+  smooth_vital(.data, {{ .var }}, smooth_fn = smooth_mortality_law_x, law = law, ...)
 }
 
-smooth_mortality_law_x <- function(data, var, age, law, ...) {
-
-  x <- data[[age]]
-
+smooth_mortality_law_x <- function(data, var, age_spacing=1, age, pop = NULL, ...) {
+  # Get Ex and Dx variables
+  Ex <- if(!is.null(pop)) data[[pop]] else NULL
   Dx_name <- vital_vars(data)["deaths"]
-  Ex_name <- vital_vars(data)["population"]
-
-  # Check if Dx Ex mx exist
   Dx <- if (!is.null(Dx_name)) data[[Dx_name]] else NULL
-  Ex <- if (!is.null(Ex_name)) data[[Ex_name]] else NULL
-  mx <- data$Mortality
-
-  if (!is.null(Dx) && !is.null(Ex)) {
-    smooth.fit <- MortalityLaws::MortalityLaw(x = x, Dx = Dx, Ex = Ex, law = law, ...)
-  } else {
-    smooth.fit <- MortalityLaws::MortalityLaw(x = x, mx = mx, law = law, ...)
+  # Need both Ex and Dx
+  if(is.null(Dx) | is.null(Ex)) {
+    Dx <- Ex <- NULL
   }
+  # Call MortalityLaws
+  smooth.fit <- MortalityLaws::MortalityLaw(x = data[[age]], Dx = Dx,
+    Ex = Ex, mx = data[[var]], ...)
   # Mean squared error
-  residual_variance <- sum((smooth.fit$residuals)^2, na.rm = TRUE) / (length(smooth.fit$fitted.values) - length(smooth.fit$coefficients))
-  standard_error <- sqrt(residual_variance)
+  n <- length(smooth.fit$fitted.values)
+  p <- length(smooth.fit$coefficients)
+  residual_variance <- sum((smooth.fit$residuals)^2, na.rm = TRUE) / (n-p)
+  # Construct output as a tibble
   out <- tibble(
-    age = x,
+    age = data[[age]],
     .smooth = smooth.fit$fitted.values,
-    .smooth_se = .smooth * standard_error / sqrt(length(x))
+    .smooth_se = .smooth * sqrt(residual_variance) / sqrt(n)
   )
   colnames(out)[1] <- age
-
-  # Extract estimated parameters
-  #param_estimates <- tibble(t(smooth.fit$coefficients))
-  #colnames(param_estimates) <- names(smooth.fit$coefficients)
-
   return(out)
 }
 
-smooth_law_vital <- function(.data, .var, smooth_fn, law, ...) {
-  if(rlang::quo_is_missing(enquo(.var))) {
-    stop("Please specify which variable to smooth. .var is missing with no default.")
-  }
-  # Index variable
-  index <- tsibble::index_var(.data)
-  # Keys including age
-  keys <- tsibble::key_vars(.data)
-  attrx <- vital_var_list(.data)
-  age <- attrx$age
-  if (is.null(age)) {
-    stop("No age variable found")
-  }
-  pop <- attrx$population
-  # Drop Age as a key and nest results
-  keys_noage <- keys[!(keys %in% c(age, "AgeGroup", "Age_Group"))]
-  # Turn .var into character
-  resp <- names(eval_select(enquo(.var), data = .data))
-  nested_data <- tidyr::nest(as_tibble(.data), .by = tidyr::all_of(c(index, keys_noage)))
-  smooth <- purrr::map(nested_data[["data"]],
-                       \(x) smooth_fn(x, resp, age, law, ...))
-  nested_data$sm <- smooth
-  nested_data$data <- NULL
-  out <- tsibble::as_tibble(nested_data) |>
-    tidyr::unnest(cols = sm) |>
-    left_join(as_tibble(.data), by = c(index, keys_noage, age))
-  cols <- c(colnames(.data), ".smooth", ".smooth_se")
-  out[cols] |>
-    tsibble::as_tsibble(index = index, key = all_of(keys)) |>
-    as_vital(
-      index = index,
-      key = all_of(keys),
-      .age = age,
-      .sex = attrx$sex,
-      .population = attrx$population,
-      .deaths = attrx$deaths,
-      .births = attrx$births,
-      reorder = TRUE
-    )
-}
 
 
